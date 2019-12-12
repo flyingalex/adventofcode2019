f5803840e4229a8ed7914be27a346ee218f71c8a
flyingalex <flyingalex@163.com>
2019-12-13 00:21:31 +0800

    finish day6 part 1

--- day6/day6.go
+++ day6/day6.go
@@ -1,1 +1,20 @@
 package day6
+
+func recursionCount(input map[string][]string, orbits []string, weight int) int {
+	count := 0
+	for _, orbit := range orbits {
+		count += weight
+		count += recursionCount(input, input[orbit], weight+1)
+	}
+	return count
+}
+
+func CaculateOrbits(input map[string][]string) int {
+	orbits := input["COM"]
+	count := 0
+	for _, orbit := range orbits {
+		count++
+		count += recursionCount(input, input[orbit], 2)
+	}
+	return count
+}
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -1,1 +1,43 @@
 package day6
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func GetInputs(path string) map[string][]string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	inputs := make(map[string][]string)
+	for scanner.Scan() {
+		text := scanner.Text()
+		input := strings.Split(text, ")")
+		inputs[input[0]] = append(inputs[input[0]], input[1])
+	}
+
+	return inputs
+}
+
+func TestCaculateOrbits(t *testing.T) {
+	t.Run("test CaculateOrbits", func(t *testing.T) {
+		inputs := GetInputs("./test.txt")
+		got := CaculateOrbits(inputs)
+		want := 42
+		if got != want {
+			log.Fatalf("want: %d, got: %d", want, got)
+		}
+	})
+	t.Run("check CaculateOrbits", func(t *testing.T) {
+		inputs := GetInputs("./input.txt")
+		got := CaculateOrbits(inputs)
+		fmt.Println(got)
+	})
+}
