ab2a80a2abe59dd2fe4b2939d47becc21e977b2c
flyingalex <flyingalex@163.com>
2019-12-15 16:47:35 +0800

    day7 part 1 refactor

--- day7/day7.go
+++ day7/day7.go
@@ -18,29 +18,22 @@ func leftPhase(used []int) []int {
 	return left
 }
 
-func FindLargestOutput(instructions []int) int {
-	phaseSetting := []int{0, 1, 2, 3, 4}
-	var results []int
-	for _, phase1 := range phaseSetting {
-		output1 := IntCodeComputer(instructions, phase1, 0)
-		leftPhases := leftPhase([]int{phase1})
-		for _, phase2 := range leftPhases {
-			output2 := IntCodeComputer(instructions, phase2, output1)
-			leftPhases := leftPhase([]int{phase1, phase2})
-			for _, phase3 := range leftPhases {
-				output3 := IntCodeComputer(instructions, phase3, output2)
-				leftPhases := leftPhase([]int{phase1, phase2, phase3})
-				for _, phase4 := range leftPhases {
-					output4 := IntCodeComputer(instructions, phase4, output3)
-					leftPhases := leftPhase([]int{phase1, phase2, phase3, phase4})
-					for _, phase5 := range leftPhases {
-						output := IntCodeComputer(instructions, phase5, output4)
-						results = append(results, output)
-					}
-				}
-			}
+func addOutput(instructions []int, used []int, input int, results *[]int) {
+	leftPhases := leftPhase(used)
+	for _, phase := range leftPhases {
+		output := IntCodeComputer(instructions, phase, input)
+		if len(used) == 4 {
+			*results = append(*results, output)
+		} else {
+			updatedUsed := append(used, phase)
+			addOutput(instructions, updatedUsed, output, results)
 		}
 	}
+}
+
+func FindLargestOutput1(instructions []int) int {
+	var results []int
+	addOutput(instructions, []int{}, 0, &results)
 	max := 0
 	for _, result := range results {
 		if result > max {
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -27,34 +27,34 @@ func TestFindLargestOutput(t *testing.T) {
 			log.Fatalf("want: %#v, got: %#v", want, got)
 		}
 	})
-	t.Run("test FindLargestOutput", func(t *testing.T) {
+	t.Run("test FindLargestOutput1", func(t *testing.T) {
 		instructions := []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}
-		got := FindLargestOutput(instructions)
+		got := FindLargestOutput1(instructions)
 		want := 43210
 		if got != want {
 			log.Fatalf("want: %d, got: %d", want, got)
 		}
 	})
-	t.Run("test FindLargestOutput", func(t *testing.T) {
+	t.Run("test FindLargestOutput1", func(t *testing.T) {
 		instructions := []int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0}
-		got := FindLargestOutput(instructions)
+		got := FindLargestOutput1(instructions)
 		want := 54321
 		if got != want {
 			log.Fatalf("want: %d, got: %d", want, got)
 		}
 	})
-	t.Run("test FindLargestOutput", func(t *testing.T) {
+	t.Run("test FindLargestOutput1", func(t *testing.T) {
 		instructions := []int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0}
-		got := FindLargestOutput(instructions)
+		got := FindLargestOutput1(instructions)
 		want := 65210
 		if got != want {
 			log.Fatalf("want: %d, got: %d", want, got)
 		}
 	})
 
-	t.Run("check FindLargestOutput result", func(t *testing.T) {
+	t.Run("check FindLargestOutput1 result", func(t *testing.T) {
 		instructions := adventofcode.GetInputsNumInOneline("./input.txt", ",")
-		got := FindLargestOutput(instructions)
+		got := FindLargestOutput1(instructions)
 		want := 92663 // result for refactor
 		if got != want {
 			log.Fatalf("want: %d, got: %d", want, got)
